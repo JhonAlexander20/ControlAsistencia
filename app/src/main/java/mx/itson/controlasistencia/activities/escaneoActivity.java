4921eb817987e58e33f82cca7735d97ee792e338
Abrilrguez <33942729+Abrilrguez@users.noreply.github.com>
2019-05-22 12:42:21 -0700

    Interfaz escaneo

--- app/src/main/java/mx/itson/controlasistencia/activities/escaneoActivity.java
+++ app/src/main/java/mx/itson/controlasistencia/activities/escaneoActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
 
-import com.journeyapps.barcodescanner.camera.CameraSurface;
 
 import mx.itson.controlasistencia.R;
 
