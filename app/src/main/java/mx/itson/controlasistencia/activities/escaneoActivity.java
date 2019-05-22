2b9b1689753b60f8bab2ade33c9664ec56b4503f
Bryant Chavez <bryant.chavez1997@gmail.com>
2019-05-22 00:39:28 -0700

    Se agrego para scanear el qr y generarlo.

--- app/src/main/java/mx/itson/controlasistencia/activities/escaneoActivity.java
+++ app/src/main/java/mx/itson/controlasistencia/activities/escaneoActivity.java
@@ -2,6 +2,9 @@ package mx.itson.controlasistencia.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
+
+import com.journeyapps.barcodescanner.camera.CameraSurface;
 
 import mx.itson.controlasistencia.R;
 
@@ -11,5 +14,6 @@ public class escaneoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escaneo);
+
     }
 }
--- app/src/main/java/mx/itson/controlasistencia/activities/listaActivity.java
+++ app/src/main/java/mx/itson/controlasistencia/activities/listaActivity.java
@@ -1,7 +1,10 @@
 package mx.itson.controlasistencia.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import mx.itson.controlasistencia.R;
